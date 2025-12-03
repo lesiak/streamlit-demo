b8ab5808ff18ed8265243f82d3407dc4fc9e66e1
lesiak <lesiak@outlook.com>
2025-12-03 16:45:40 +0100

    Text output

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from streamlit-demo!")
+import streamlit as st
 
+def main():
+    st.title("Hello from streamlit-demo!")
+    typography()
+
+def typography():   
+    st.header("How is it going?")
+    st.subheader("Typography")
+
+    st.text("Regular text")
+    "text without function call"
+    1 + 2
+
+    st.text("st.echo")
+    with st.echo():
+        1 + 2
+
+    st.divider()
+    st.badge("New")
 
 if __name__ == "__main__":
     main()
